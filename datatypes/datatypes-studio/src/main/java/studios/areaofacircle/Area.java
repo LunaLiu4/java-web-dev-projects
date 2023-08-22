db6a6b26e4cc946ce4fda46fcacc2906958bcbf4
LunaLiu4 <luna.liu829@gmail.com>
2023-08-22 09:50:57 -0500

    studio 1 bonus

--- datatypes/datatypes-studio/src/main/java/studios/areaofacircle/Area.java
+++ datatypes/datatypes-studio/src/main/java/studios/areaofacircle/Area.java
@@ -3,13 +3,28 @@ package studios.areaofacircle;
 import java.util.Scanner;
 
 public class Area {
+    static Object userInput;
+    static Object classofInput;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double radius;
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-//        double area = radius * radius * 3.14;
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius 2.5 is: " + area);
+        boolean result = input.hasNextDouble();
+        if (!result){
+            System.out.println("Invalid enter!");
+        }
+        else{
+            radius = input.nextDouble();
+            if ( radius > 0){
+                double area = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius " + radius + " is: "+ area);
+            }
+            else {
+                System.out.println("Invalid enter!");
+            }
+        }
+        input.close();
+       }
     }
 
-}
